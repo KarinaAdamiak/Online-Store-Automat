dcf2a4d72cc4b385727c877be061b180a47d2af5
Karina Adamiak <kara.ada1985@gmail.com>
2023-02-04 14:08:17 +0100

    Adding click on My account

--- cypress/e2e/page-objects/homePage.js
+++ cypress/e2e/page-objects/homePage.js
@@ -1,8 +1,13 @@
 import Urls from "./urlsPage";
 
-
+const myAccountHeaderButton= '#menu-item-100'
 class HomePage{
     
+clickMyAccountButton(){
+    cy.get(myAccountHeaderButton).click()
+}
+
+
 visitPage(){
     const url= new Urls()     //object
     url.visitHomePage()
