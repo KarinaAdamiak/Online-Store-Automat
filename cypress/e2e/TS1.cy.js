61850960eb18c14da0f9263693f33367c9e47d20
Karina Adamiak <kara.ada1985@gmail.com>
2023-02-04 17:40:03 +0100

    Change

--- cypress/e2e/TS1.cy.js
+++ cypress/e2e/TS1.cy.js
@@ -16,21 +16,22 @@ cy.fixture('users.json').as('userData')
 
 })
 
-// it('should login to the application', function() {
-//     homePage.visitPage()
-//     homePage.clickMyAccountButton()
-//     accountPage.fillUserNameEmailInputWithEmail(this.userData.email)
-//     accountPage.fillPasswordField(this.userData.password)
-//     accountPage.clickLoginButton()
-//     accountPage.checkVisibilityOfMyAccountNavigation()
-//})
+it('should login to the application', function() {
+    homePage.visitPage()
+    homePage.clickMyAccountButton()
+    accountPage.fillUserNameEmailInputWithEmail(this.userData.email)
+    accountPage.fillPasswordField(this.userData.password)
+    accountPage.clickLoginButton()
+    accountPage.checkVisibilityOfMyAccountNavigation()
+})
 
-// it('should not login to the application', function() {
-//     accountPage.visitMyAccountPage()
-//     accountPage.fillUserNameEmailInputWithEmail(faker.internet.email())
-//     accountPage.fillPasswordField(faker.internet.password())
-//     accountPage.clickLoginButton()
-//     accountPage.checkVisibilityOfErrorMessageAfterIncorrectLogin()
+it('should not login to the application', function() {
+    accountPage.visitMyAccountPage()
+    accountPage.fillUserNameEmailInputWithEmail(faker.internet.email())
+    accountPage.fillPasswordField(faker.internet.password())
+    accountPage.clickLoginButton()
+    accountPage.checkVisibilityOfErrorMessageAfterIncorrectLogin()
+})
 
 it('should add product to cart and delete it from there', function() {
     homePage.visitPage()
